b270270887198c3269d6607fb9d66c4856d29525
leedy-dev <ldy033000@gmail.com>
2024-02-27 23:16:01 +0900

    first commit

---
+++ src/main/java/com/dyprj/mitd/MitdosServerApplication.java
@@ -0,0 +1,13 @@
+package com.dyprj.mitd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitdosServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitdosServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyprj/mitd/MitdosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyprj.mitd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitdosServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
